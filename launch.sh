dcff4fd757cf2defcf484509c23ed797544c5be5
Maxime Sibellas <maxime.sibellas@ekino.com>
2021-10-08 11:56:40 +0200

    Patch nginx service to use a loadbalancer

--- launch.sh
+++ launch.sh
@@ -2,6 +2,8 @@
 
 echo "Starting cluster"
 minikube start
+# Needed to tell metallb to create a loadbalancer and redirect traffic to nginx
+kubectl patch service ingress-nginx-controller --type=merge -p "spec: {type: LoadBalancer}" -n ingress-nginx
 echo "Export docker configuration"
 source <(echo $(minikube -p localdev docker-env))
 echo "Start coding"
